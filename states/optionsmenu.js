200550f98a1275e18608b345e6930e66e55ea7cb
Adrian Wong <Adrian Wong>
2017-04-26 21:53:48 -0700

    Add panel template and single back button.

--- states/optionsmenu.js
+++ states/optionsmenu.js
@@ -1,5 +1,16 @@
 function openOptionsMenu() {
-    game.backgroundColor = 0x555500;
+	let optionsGroup = game.group();
+	let optionsPanel = game.rectangle(200,400,"grey");
+	let backButton = game.circle(50,"red");
+	optionsGroup.addChild(optionsPanel, backButton);
+	game.stage.putCenter(optionsGroup);
+	backButton.interact = true;
+	
+	backButton.tap = () => {
+		game.stage.remove(optionsGroup);
+	}
+	
+	game.state = optionsMenu;
 }
 
 function optionsMenu() {
