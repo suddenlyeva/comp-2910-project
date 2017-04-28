e24269f3f236bb9f793123e374c98d58fba2414e
EvaRia-Git <adrian.wong95@gmail.com>
2017-04-28 10:31:49 -0700

    Add state flow between stages through stagecomplete screen

--- states/stagecomplete.js
+++ states/stagecomplete.js
@@ -1,7 +1,23 @@
 "use strict";
 
+// This should probably be part of the level object;
+
 function openStageComplete() {
     game.backgroundColor = 0x000044;
+    
+	let completionGroup = game.group();
+	let completionPanel = game.rectangle(200,400,"grey");
+	let continueButton = game.circle(50,"green");
+	completionGroup.addChild(completionPanel, continueButton);
+	game.stage.putCenter(completionGroup);
+	continueButton.interact = true;
+	
+	continueButton.tap = () => {
+		continueButton.enabled = false;
+		game.stage.remove(completionGroup);
+        nextStage();
+		game.state = STAGES[currentStage];
+	}
     game.state = stageComplete;
 }
 
--- states/stages.js
+++ states/stages.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Needs heavy refactoring
+
 let STAGES = [setupStage0, setupStage1, setupStage2, setupStage3, setupStage4];
 let currentStage = 0;
 let stageTimer;
@@ -22,43 +24,70 @@ function stage0() {
 }
 
 function setupStage1() {
-    game.backgroundColor = 0x000000;
+    game.backgroundColor = 0x330000;
     currentStage = 1;
     console.log("stage 1");
+    let background = game.sprite("background.png");
+    stageTimer = 0;
     game.state = stage1;
 }
 
 function stage1() {
+    stageTimer++;
+    if (stageTimer === 120) {
+        game.state = openStageComplete;
+    }
 }
 
 function setupStage2() {
-    game.backgroundColor = 0x000000;
+    game.backgroundColor = 0x660000;
     currentStage = 2;
     console.log("stage 2");
+    let background = game.sprite("background.png");
+    stageTimer = 0;
+    
     game.state = stage2;
 }
 
 function stage2() {
+    stageTimer++;
+    if (stageTimer === 120) {
+        game.state = openStageComplete;
+    }
 }
 
 function setupStage3() {
-    game.backgroundColor = 0x000000;
+    game.backgroundColor = 0x990000;
     currentStage = 3;
     console.log("stage 3");
+    let background = game.sprite("background.png");
+    stageTimer = 0;
+    
     game.state = stage3;
 }
 
 function stage3() {
+    stageTimer++;
+    if (stageTimer === 120) {
+        game.state = openStageComplete;
+    }
 }
 
 function setupStage4() {
-    game.backgroundColor = 0x000000;
+    game.backgroundColor = 0xcc0000;
     currentStage = 4;
     console.log("stage 4");
+    let background = game.sprite("background.png");
+    stageTimer = 0;
+    
     game.state = stage4;
 }
 
 function stage4() {
+    stageTimer++;
+    if (stageTimer === 120) {
+        game.state = openStageComplete;
+    }
 }
 
 function nextStage() {
