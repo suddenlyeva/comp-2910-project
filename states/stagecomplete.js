99d3905b0efb64af88841381934787ea65d5bebb
EvaRia-Git <adrian.wong95@gmail.com>
2017-04-29 16:27:56 -0700

    stage completion to new style

--- states/stagecomplete.js
+++ states/stagecomplete.js
@@ -2,23 +2,46 @@
 
 // This should probably be part of the level object;
 
+let stageCompleteScene;
+
 function openStageComplete() {
-    game.backgroundColor = 0x000044;
     
-	let completionGroup = game.group();
-	let completionPanel = game.rectangle(200,400,"grey");
-	let continueButton = game.circle(50,"green");
-	completionGroup.addChild(completionPanel, continueButton);
-	game.stage.putCenter(completionGroup);
-	continueButton.interact = true;
-	
-	continueButton.tap = () => {
-		continueButton.enabled = false;
-		game.stage.remove(completionGroup);
-        nextStage();
-		game.state = STAGES[currentStage];
-	}
-    game.state = stageComplete;
+    // First Initialize Only
+    if(stageCompleteScene == null) {
+        
+        // Make Panel and Buttons
+        let panel = game.rectangle(200,400,"grey");
+        let backButton = game.circle(50,"red");
+        let continueButton = game.circle(50,"green");
+        backButton.interact = true;
+        continueButton.interact = true;
+        
+        // Add to scene
+        stageCompleteScene = game.group();
+        stageCompleteScene.buttons = groupButtons(backButton,continueButton);
+        stageCompleteScene.addChild(panel);
+        stageCompleteScene.addChild(stageCompleteScene.buttons);
+        
+        game.stage.addChild(stageCompleteScene);
+        
+        // Position stuff
+        game.stage.putCenter(stageCompleteScene);
+        backButton.x += 50;
+        
+        // Continue button moves to main menu
+        continueButton.tap = () => {
+            stageCompleteScene.visible = false;
+            stageCompleteScene.buttons.disable();
+            nextStage();
+            game.state = STAGES[currentStage];
+        }
+    }
+    
+    // Every time opened
+    stageCompleteScene.visible = true;
+    stageCompleteScene.buttons.enable();
+
+	game.state = stageComplete;
 }
 
 function stageComplete() {
