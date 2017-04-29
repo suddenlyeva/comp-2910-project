ea21b6480d8a7b2d6d5e3e1247758fc2e10cf4d2
EvaRia-Git <adrian.wong95@gmail.com>
2017-04-29 16:15:34 -0700

    stage select to new style

--- states/stageselect.js
+++ states/stageselect.js
@@ -1,25 +1,47 @@
 "use strict";
 
+let stageSelectScene;
+
 function openStageSelect() {
-    game.backgroundColor = 0x005555;
 
-    let stageGroup = game.group();
-    let stageBtns = [];
-    for(let i = 0; i < STAGES.length; i++) {
-        stageBtns.push(game.circle(50, "blue"));
-        stageGroup.addChild(stageBtns[i]);
-        stageBtns[i].position.set(300, i * 60 + 60);
-        stageBtns[i].interact = true;
+    // First initialize only
+    if(stageSelectScene == null) {
+        
+        // Declare an array of buttons
+        let stageBtns = [];
+        
+        // Make scene groups
+        stageSelectScene = game.group();
+        stageSelectScene.buttons = groupButtons();
+        stageSelectScene.addChild(stageSelectScene.buttons);
+        game.stage.addChild(stageSelectScene);
+        
+        // Initialize buttons
+        for(let i = 0; i < STAGES.length; i++) {
+            
+            // Create
+            stageBtns.push(game.circle(50, "blue"));
+            stageBtns[i].interact = true;
+            
+            // Add to group
+            stageSelectScene.buttons.addChild(stageBtns[i]);
+            
+            // Position
+            stageBtns[i].position.set(300, i * 60 + 60);
 
-        stageBtns[i].tap = () => {
-			for(let btn in stageBtns) {
-				stageBtns[btn].enabled = false;
-			}
-            game.remove(stageGroup);
-            game.state = STAGES[i];
-        };
+            // Set behaviour
+            stageBtns[i].tap = () => {
+                stageSelectScene.visible = false;
+                stageSelectScene.buttons.disable();
+                game.state = STAGES[i];
+            };
+        }
     }
-
+    
+    // Every time opened
+    game.backgroundColor = 0x005555;
+    stageSelectScene.buttons.enable();
+    
     game.state = stageSelect;
 }
 
