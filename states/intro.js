cbed66a4e1634ae5f58c5c87b746908329f6c6bb
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-05-02 20:54:24 -0700

    Styled words Food and Factory

--- states/intro.js
+++ states/intro.js
@@ -4,20 +4,31 @@ let introScene;
 
 function Intro() {
     this.scene = new PIXI.Container();
-    this.txtFood = new PIXI.Text(
-        "FOOD",
-        {fontFamily: "Arial", fontSize: 256, fill: 0x00ad5e, align: "center"}
-    );
-    this.txtFactor = new PIXI.Text(
-    
-    );
-    this.txtZero = new PIXI.Text(
-    
+    this.txtFood = new PIXI.Text("FOOD", {
+        fontFamily: "Arial", fontSize: 128, fill: 0x00ad5e,
+        dropShadow: true, dropShadowAngle: 7 * Math.PI / 12, dropShadowDistance: 10,
+        fontWeight: "bold", stroke: 0xFFFFFF, strokeThickness: 7
+    });
+
+    this.txtFactory = new PIXI.Text("FACTORY", this.txtFood.style);
+
+    this.txtZero = new PIXI.Text("ZERO",
+        {fontFamily: "Arial", fontSize: 74, fill: 0xc8543b}
     );
 
+    // Position relatively
+    this.txtFood.position.set(RENDERER.width / 2 - this.txtFood.width / 2, 130);
+    this.txtFactory.position.set(RENDERER.width / 2 - this.txtFactory.width / 2,
+        this.txtFood.y + this.txtFood.height + 20);
+    this.txtZero.y = this.txtFactory.y + this.txtFactory.height + 20;
+
     this.update = function() {
-        
+
     };
+
+    this.scene.addChild(this.txtFood);
+    this.scene.addChild(this.txtFactory);
+    this.scene.addChild(this.txtZero);
 }
 
 function openIntro() {
