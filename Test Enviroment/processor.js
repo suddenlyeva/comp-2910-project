39dce1bbee8f90037404005af7bfdfedf8dd0cce
ccyang1991 <ccyang1991@hotmail.com>
2017-05-03 12:06:18 -0700

    updated the pseudocode
    - Joseph

--- Test Enviroment/processor.js
+++ Test Enviroment/processor.js
@@ -1,69 +1,148 @@
 "use strict";
 
+import spriteManager from './location';
+
+@class
+
 function Processor(recipeOrder) //the Recipe this Processor will produce 
 {
+	//-------------------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------------------
+	
+	//-------------------------------------------------------------------------------
+	// Object Variables
+	//-------------------------------------------------------------------------------
+	this.mSprite; 				// Sprite Variable
+	this.requiredIngredients;	// array of required items
+	this.numIngredients;		// Total Number of Ingredients
+	this.recipeProgress; 		// An array of booleans to check if each seperate ingredients are placed
+	this.mPosition;
+	
+	this.totalProcessTime = 60; 		// total time it takes for an item to process
+	this.processTimer = 0; 				// Current process duration	
+	this.timerCircle;					// Drawing Circle
+	
+	
+	
+	this.isCollidable = true;
+	
+	
+	// 0 = loading items
+	// 1 = processing
+	this.processorState = 0; 
 
-	//two possible ways to pull list
-	this.requiredIngredients = [recipeOrder.getIngredentsList()]; //Pulls list of item array?
-	this.requiredIngredients = recipeOrder;
+	//-------------------------------------------------------------------------------
+	// Project handler functions
+	//-------------------------------------------------------------------------------
 	
-	this.numIngredients = requiredIngredients.length - 1;	// Total Number of Ingredients
-	
-	// based of the number of ingredients that are in the recipe
-	this.recipeProgress = new Array(numIngredients);
-	/* 
-	//Forcefully initiates them to false
-	for(var i = 0; i < requiredIngredeints.length; ++i)
-	{
-		recipeProgress[i] = false;
-	}
-	*/
-	
-	//defaults to 60 seconds for a processor the finished producing the item
-	this.totalProcessTime = 60; 
-	
-	//current process duration
-	this.processTimer = 0; 	
-	
-	this.position = new position(0,0);
-	this.processorState
-	
+	// On create 
+	// Variable assignment
 	this.OnSetup : function() 
 	{
+		this.mSprite = spriteManager.getSprite("Processor");  // example
+	
+		// Sets Position
+		this.mPosition = new Position(0,0);
+		this.boundingBox = new BoundingBox();	// No spritemanager to handle collision checks between all sprites.
 		
+		// load all the sprites for the ingredients
+		
+		
+		// Two possible ways to pull list
+		this.requiredIngredients = [recipeOrder.getIngredentsList()]; // Pulls list of item array?
+		this.requiredIngredients = recipeOrder;
+		
+		this.numIngredients = requiredIngredients.length - 1;	// Assigns total number of ingredients
+		
+		this.recipeProgress = new Array(numIngredients);
+		
+		for(var i = 0; i < requiredIngredeints.length; ++i) // Forcefully initiates them to false
+		{
+			recipeProgress[i] = false;
+		}
 	}
 	
+	//-------------------------------------------------------------------------------
+	// On draw
+	this.OnRender : function()
+	{
+			//Stuffs to do on Draw?
+			
+			//if in processing state 
+				stage.addChild(timerCircle)
+			//render clock
+			
+	}
 	
+	//-------------------------------------------------------------------------------
+	// On Update
 	this.OnUpdate : function() 
 	{
 		
 		if(!bRecipeCompletion)
 		{
-			
+			processState = 1;
 		}
-		else	//if fully completed
+		switch (processState)
 		{
-			
-		}
+			case 0: // still loading Ingredients on the tray
+			{
+				
+				// Iterates through all the items on the scene and see if one is "dropped" into the processor
+				if(itemCheck(collidedObject))
+				{
+					// Kill dropped object Item
+					// grayed out object becomes fully drawn
+				}
+					
+			}break;
+			case 1: // Processing Ingredients
+			{		
+				timerUpdate();
+				//do something related to processing
+			}break;
+		} // Switch
 		
 	}
 	
+	//-------------------------------------------------------------------------------
+	// Memeber functions
+	//-------------------------------------------------------------------------------
 	
-	//checks if dragged item is an item on the recipe
+	
+	//-------------------------------------------------------------------------------
+	// Updates the timer
+	
+	this.timerUpdate : function()
+	{
+		if(processTimer < totalProcessTime)
+		{
+			processTimer += deltaTime;
+		}
+	}
+	
+	//-------------------------------------------------------------------------------
+	// Checks if dragged item is an item on the recipe
 	this.itemCheck(droppedObj)
 	{
-		for(var i = 0; i < numIngredients; ++i)
+		for(let i = 0; i < numIngredients; ++i)
 		{
+			
 			if(requiredIngredients[i] === droppedObj.getName())
 			{
+				requiredIngredients[i] = true; // if found flip to true 
 				return true;
 			}
 		}
 		return false;
 	}
+	
+	//-------------------------------------------------------------------------------
+	// Checks if all the required Recipe Ingredients are added
 	this.bRecipeCompletion : function()
 	{
-		for (var i = 0; i < numIngredients; ++i)
+		for (let i = 0; i < numIngredients; ++i)
 		{
 			if(requiredIngredients[i] === false)
 				return false;
@@ -71,18 +150,20 @@ function Processor(recipeOrder) //the Recipe this Processor will produce
 		return true;
 	}
 	
+	//-------------------------------------------------------------------------------
+
 }
 
-//project defined position placeholder
-function position(x1,y1)
+// Position class handler
+function Position(x1,y1)
 {
 	this.x = x1;
 	this.y = y1;
 	
-	getX : function() { return x;}
-	getY : function() { return y;}
+	this.getX : function() { return x;}
+	this.getY : function() { return y;}
 	
-	movePos : function(x1, y1) 
+	this.movePos : function(x1, y1) 
 	{ 
 		x += x1; 
 		y += y1;
