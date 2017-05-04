e6a702b71a930edfe63bf285e2d6d872068e52ae
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-05-04 11:34:33 -0700

    Adjusted intro font style to work with our custom font

--- states/intro.js
+++ states/intro.js
@@ -5,30 +5,30 @@ function Intro() {
 
     // Define and style text
     this.txtFood = new PIXI.Text("FOOD", {
-        fontFamily: FONT_FAMILY, fontSize: 128, fill: 0x00ad5e,
+        fontFamily: FONT_FAMILY, fontSize: 256, fill: 0x00ad5e,
         dropShadow: true, dropShadowAngle: 7 * Math.PI / 12, dropShadowDistance: 10,
-        fontWeight: "bold", stroke: 0xFFFFFF, strokeThickness: 7
+        stroke: 0xFFFFFF, strokeThickness: 7
     });
 
     this.txtFactory = new PIXI.Text("FACTORY", this.txtFood.style);
 
     this.txtZero = new PIXI.Text("ZERO", this.txtFood.style.clone());
-    this.txtZero.style.fontSize = 74;
+    this.txtZero.style.fontSize = 148;
     this.txtZero.style.fill = 0xff3333;
     this.txtZero.style.strokeThickness = 4;
 
     this.txtPress = new PIXI.Text("Press anywhere to continue...", {
-        fontFamily: FONT_FAMILY, fontSize: 48, fill: 0xbd00ff,
-        fontWeight: "bold", stroke: 0xFFFFFF, strokeThickness: 7
+        fontFamily: FONT_FAMILY, fontSize: 96, fill: 0xbd00ff,
+        stroke: 0xFFFFFF, strokeThickness: 7
     });
 
     this.txtTeam19 = new PIXI.Text("By team19", {
-        fontFamily: FONT_FAMILY, fontSize: 32, fill: 0x333333, strokeThickness: 1
+        fontFamily: FONT_FAMILY, fontSize: 64, fill: 0x333333, strokeThickness: 1
     });
 
     // Position text
     // note no padding between lines
-    this.txtFood.position.set(RENDERER.width / 2 - this.txtFood.width / 2, 130);
+    this.txtFood.position.set(RENDERER.width / 2 - this.txtFood.width / 2, 0);
     this.txtFactory.position.set(RENDERER.width / 2 - this.txtFactory.width / 2,
         this.txtFood.y + this.txtFood.height);
     this.txtZero.position.set(RENDERER.width / 2 - this.txtZero.width / 2,
