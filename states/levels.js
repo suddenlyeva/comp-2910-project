68d4665152205fc70cd3972802769336d7eab104
EvaRia-Git <adrian.wong95@gmail.com>
2017-05-04 13:03:57 -0700

    revive pause button

--- states/levels.js
+++ states/levels.js
@@ -21,8 +21,16 @@ let LEVELS = [
 
 function Level(data) {
     this.scene = new PIXI.Container();
+    
     this.background = new PIXI.Sprite(PIXI.utils.TextureCache["background.png"]);
     this.scene.addChild(this.background);
+    
+    
+    this.pauseButton = makeSimpleButton(100, 50, "pause", 0x94b8b8, 50);
+    this.pauseButton.position.set(CANVAS_WIDTH - 150, 100);
+    this.pauseButton.on("pointertap", PauseMenu.open);
+    this.scene.addChild(this.pauseButton);
+    
     this.levelNumber = data.id;
     this.name = data.name;
     this.processors = data.processors;
