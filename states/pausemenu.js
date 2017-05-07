b13271be5aaeed5dacffdb0eedb2a2f9e74da35b
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-05-07 15:28:08 -0700

    Added panel for buttons

--- states/pausemenu.js
+++ states/pausemenu.js
@@ -1,7 +1,9 @@
 "use strict";
 
 function PauseMenu() {
-    this.scene = new PIXI.Container();
+    this.width  = 800;
+    this.height = 600;
+    this.scene  = new PIXI.Container();
     // holds the state before the pause
     this.stateBuffer;
 
@@ -11,16 +13,17 @@ function PauseMenu() {
         STATE = this.update;
     });
 
-    // Make Buttons
-    this.resumeButton = makeSimpleButton(100, 50, "resume", 0xb3ecec, 50);
-    this.optionsButton = makeSimpleButton(100, 50, "options", 0x94b8b8, 50);
-    this.mainMenuButton = makeSimpleButton(100, 50, "main menu", 0xfff3ad, 50);
-    this.resetButton = makeSimpleButton(100, 50, "reset", 0xfff3ad, 50);
+    this.panel = new PIXI.Graphics();
+    this.panel.lineStyle(1, 0, 1);
+    this.panel.beginFill(0xfff3ad);
+    this.panel.drawRect(0, 0, this.width, this.height);
+    this.panel.endFill();
 
-    // Make background elements and add them to the background container
-    this.background = new PIXI.Container();
-    // prevent the scene under from being clickable
-    this.background.interactive = true;
+    // Make Buttons
+    this.resumeButton   = makeSimpleButton(100, 50, "resume"   , 0xb3ecec, 50);
+    this.optionsButton  = makeSimpleButton(100, 50, "options"  , 0x94b8b8, 50);
+    this.mainMenuButton = makeSimpleButton(100, 50, "main menu", 0xfff3ad, 50);
+    this.resetButton    = makeSimpleButton(100, 50, "reset"    , 0xfff3ad, 50);
 
     this.bgFill = new PIXI.Graphics();
     this.bgFill.beginFill(0x6a0e1d);
@@ -28,16 +31,24 @@ function PauseMenu() {
     this.bgFill.alpha = 0.4;
     this.bgFill.endFill();
 
+
+    // Make background elements and add them to the background container
+    this.background = new PIXI.Container();
+    // prevent the scene under from being clickable
+    this.background.interactive = true;
     this.background.addChild(this.bgFill);
 
     // Add to scene
     this.scene.addChild(this.background);
+    this.scene.addChild(this.panel);
     this.scene.addChild(this.resumeButton);
     this.scene.addChild(this.resetButton);
     this.scene.addChild(this.optionsButton);
     this.scene.addChild(this.mainMenuButton);
 
     // Position Buttons
+    this.panel.position.set(CANVAS_WIDTH  / 2 - this.panel.width  / 2,
+                            CANVAS_HEIGHT / 2 - this.panel.height / 2);
     this.resumeButton.position.set(150, 200);
     this.resetButton.position.set(300, 200);
     this.optionsButton.position.set(150, 300);
