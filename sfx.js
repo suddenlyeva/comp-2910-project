a958bee260a8790fd330ac413b1be80dccffe1bc
ccyang1991 <ccyang1991@hotmail.com>
2017-05-17 11:16:29 -0700

    Added Functions to sfx.js
    - Joseph

--- sfx.js
+++ sfx.js
@@ -15,7 +15,7 @@ let eSFXList = {
     Splat: "sounds/splat.wav",
     Processing : "sounds/processing.ogg",
     ClockTicking : "sounds/ticking.wav",
-
+    IntoProcessor : "sounds/into-processor.wav"
 };
 
 // Loads the sound effects
@@ -25,17 +25,31 @@ sounds.load([
     eSFXList.ItemPickUp,
     eSFXList.Splat,
     eSFXList.Processing,
-    eSFXList.ClockTicking
+    eSFXList.ClockTicking,
+    eSFXList.IntoProcessor
 ]);
 
 // Adds the sounds to SFX master to allow volume control of sfx
 sounds.whenLoaded = () => {
+    console.log("test");
     SFX_MASTER.push(sounds[eSFXList.MenuOpen]);
     SFX_MASTER.push(sounds[eSFXList.ButtonClick]);
     SFX_MASTER.push(sounds[eSFXList.ItemPickUp]);
     SFX_MASTER.push(sounds[eSFXList.Splat]);
     SFX_MASTER.push(sounds[eSFXList.Processing]);
     SFX_MASTER.push(sounds[eSFXList.ClockTicking]);
+    SFX_MASTER.push(sounds[eSFXList.IntoProcessor]);
+};
+
+function PlaySound(sfx, isLooping) {
+    sounds[sfx].loop = isLooping;
+    sounds[sfx].play();
+};
+
+function StopSound(sfx) {
+    sounds[sfx].playFrom(0);    // Resets the player back to time 0
+    sounds[sfx].pause();        // Pauses it right after reseting playbar
+    sounds[sfx].loop = false;   // In case if it is flagged as looping
 };
 
 
