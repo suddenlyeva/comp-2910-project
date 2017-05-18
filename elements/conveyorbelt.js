278c8177bf6b77dc8fc8ec059c1b76f0051910d9
EvaRia-Git <adrian.wong95@gmail.com>
2017-05-18 13:59:50 -0700

    change condition

--- elements/conveyorbelt.js
+++ elements/conveyorbelt.js
@@ -121,43 +121,43 @@ function ConveyorBelt(itemTypes, speed, level) { // <- elements/ingredient.js, s
     }
     
     
-    // PPAP
-    let pen = new PIXI.Sprite(ITEM_TEXTURES[PEN]);
-    pen.anchor.set(0.5);
-    pen.interactive = true;
-    pen.buttonMode = true;
-    pen.x = CANVAS_WIDTH - 0.5 * TILES_PX;;
-    pen.y = CANVAS_HEIGHT - 60;
-    level.scene.addChild(pen);
-    
-        
-    pen.onDragStart = (event) => {
-        if (!level.itemPickedUp) { // -> states/levels.js
-            PlaySound(eSFXList.ItemPickUp, false);
-            pen.data = event.data;
-            pen.alpha = 0.5;
-            pen.dragging = true;
-            level.itemPickedup = true;
-        }
-    };
-    
-    pen.onDragMove = () => {
-        if(pen.dragging) {
-            // Track x and y
-            let newPosition = pen.data.getLocalPosition(pen.parent);
-            pen.x = newPosition.x;
-            pen.y = newPosition.y;
-        }
-    };
-    
-    pen.onDragEnd = () => {
-
-        if(pen.dragging) {
-            if (pen.x < TILES_PX && pen.y < TILES_PX) {
-                PlaySound(eSFXList.IntoProcessor, false);
-                level.scene.removeChild(pen);
                 
-                if(!PPAP_UNLOCKED) {
+    if(!PPAP_UNLOCKED) {
+        // PPAP
+        let pen = new PIXI.Sprite(ITEM_TEXTURES[PEN]);
+        pen.anchor.set(0.5);
+        pen.interactive = true;
+        pen.buttonMode = true;
+        pen.x = CANVAS_WIDTH - 0.5 * TILES_PX;;
+        pen.y = CANVAS_HEIGHT - 60;
+        level.scene.addChild(pen);
+        
+            
+        pen.onDragStart = (event) => {
+            if (!level.itemPickedUp) { // -> states/levels.js
+                PlaySound(eSFXList.ItemPickUp, false);
+                pen.data = event.data;
+                pen.alpha = 0.5;
+                pen.dragging = true;
+                level.itemPickedup = true;
+            }
+        };
+        
+        pen.onDragMove = () => {
+            if(pen.dragging) {
+                // Track x and y
+                let newPosition = pen.data.getLocalPosition(pen.parent);
+                pen.x = newPosition.x;
+                pen.y = newPosition.y;
+            }
+        };
+        
+        pen.onDragEnd = () => {
+
+            if(pen.dragging) {
+                if (pen.x < TILES_PX && pen.y < TILES_PX) {
+                    PlaySound(eSFXList.IntoProcessor, false);
+                    level.scene.removeChild(pen);
                     LEVELS.push(PPAP);
                     LEVEL_PROGRESS.push({
                         unlocked: true,
@@ -165,25 +165,26 @@ function ConveyorBelt(itemTypes, speed, level) { // <- elements/ingredient.js, s
                     });
                     PPAP_UNLOCKED = true;
                 }
+                
+                PlaySound(eSFXList.ItemDropped, false);
+                this.itemPickedup = false;
             }
-            
-            PlaySound(eSFXList.ItemDropped, false);
-            this.itemPickedup = false;
-        }
-    
-        // Reset visuals and flag
-        pen.alpha = 1;
-        pen.dragging = false;
-        pen.data = null;
         
-    };
-    
-    // Declare event handlers
-    pen.on('pointerdown', pen.onDragStart)
-        .on('pointerup', pen.onDragEnd)
-        .on('pointerupoutside', pen.onDragEnd)
-        .on('pointermove', pen.onDragMove);
+            // Reset visuals and flag
+            pen.alpha = 1;
+            pen.dragging = false;
+            pen.data = null;
+            
+        };
+        
+        // Declare event handlers
+        pen.on('pointerdown', pen.onDragStart)
+            .on('pointerup', pen.onDragEnd)
+            .on('pointerupoutside', pen.onDragEnd)
+            .on('pointermove', pen.onDragMove);
 
+    }
+    
     // Trash Pit
     let trashPit = new PIXI.Sprite(
             PIXI.loader.resources["images/spritesheet.json"].textures["trash-pit.png"]
