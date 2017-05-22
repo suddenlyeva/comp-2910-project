dd05f989e0d205f4d4911cad906789eba01472e7
Keishi Asai <isheik808@gmail.com>
2017-05-22 12:53:42 -0700

    Delete unnecessary code and file

--- web/auth/app.js
+++ web/auth/app.js
@@ -17,18 +17,20 @@
  */
 // FirebaseUI config.
 let uiConfig = {
-  'signInSuccessUrl': '../../play.html',
-  'callbacks': {
-    // Called when the user has been successfully signed in.
-    'signInSuccess': function(user, credential, redirectUrl) {
-      handleSignedInUser(user);
-        console.log("singInSuccessCalled");
-        return true;
-    }
-  },
-  'signInOptions': [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ]
+    // The url to redirect when user successfully logged in
+    'signInSuccessUrl': '../../play.html',
+    // Call back functions
+    'callbacks': {
+        // Called when the user has been successfully signed in.
+        'signInSuccess': function (user, credential, redirectUrl) {
+            handleSignedInUser(user);
+            // The flag to activate the redirect to signInSuccessUrl
+            return true;
+        }
+    },
+    'signInOptions': [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ]
 };
 
 // Initialize the FirebaseUI Widget using Firebase.
@@ -37,98 +39,45 @@ let ui = new firebaseui.auth.AuthUI(firebase.auth());
 let currentUid = null;
 
 /**
- * Redirects to the FirebaseUI widget.
- */
-let signInWithRedirect = function() {
-  window.location.assign('widget.html');
-};
-
-
-/**
- * Open a popup with the FirebaseUI widget.
- */
-let signInWithPopup = function() {
-  window.open('widget.html', 'Sign In', 'width=985,height=735');
-};
-
-
-/**
  * Displays the UI for a signed in user.
  * @param {!firebase.User} user
  */
-let handleSignedInUser = function(user) {
-  currentUid = user.uid;
-  document.getElementById('user-signed-in').style.display = 'block';
-  document.getElementById('user-signed-out').style.display = 'none';
-  document.getElementById('name').textContent = user.displayName;
-  document.getElementById('email').textContent = user.email;
-  // if (user.photoURL){
-  //   document.getElementById('photo').src = user.photoURL;
-  //   document.getElementById('photo').style.display = 'block';
-  // } else {
-  //   document.getElementById('photo').style.display = 'none';
-  // }
+let handleSignedInUser = function (user) {
+    currentUid = user.uid;
+    document.getElementById('user-signed-in').style.display = 'block';
+    document.getElementById('user-signed-out').style.display = 'none';
 };
-
 
 /**
  * Displays the UI for a signed out user.
  */
-let handleSignedOutUser = function() {
-  document.getElementById('user-signed-in').style.display = 'none';
-  document.getElementById('user-signed-out').style.display = 'block';
-  ui.start('#firebaseui-container', uiConfig);
+let handleSignedOutUser = function () {
+    document.getElementById('user-signed-in').style.display = 'none';
+    document.getElementById('user-signed-out').style.display = 'block';
+    ui.start('#firebaseui-container', uiConfig);
 };
 
 // Listen to change in auth state so it displays the correct UI for when
 // the user is signed in or not.
-firebase.auth().onAuthStateChanged(function(user) {
-  // The observer is also triggered when the user's token has expired and is
-  // automatically refreshed. In that case, the user hasn't changed so we should
-  // not update the UI.
-  if (user && user.uid == currentUid) {
-    return;
-  }
-  document.getElementById('loading').style.display = 'none';
-  document.getElementById('loaded').style.display = 'block';
-  user ? handleSignedInUser(user) : handleSignedOutUser();
-});
-
-/**
- * Deletes the user's account.
- */
-let deleteAccount = function() {
-  firebase.auth().currentUser.delete().catch(function(error) {
-    if (error.code == 'auth/requires-recent-login') {
-      // The user's credential is too oldfi. She needs to sign in again.
-      firebase.auth().signOut().then(function() {
-        // The timeout allows the message to be displayed after the UI has
-        // changed to the signed out state.
-        setTimeout(function() {
-          alert('Please sign in again to delete your account.');
-        }, 1);
-      });
+firebase.auth().onAuthStateChanged(function (user) {
+    // The observer is also triggered when the user's token has expired and is
+    // automatically refreshed. In that case, the user hasn't changed so we should
+    // not update the UI.
+    if (user && user.uid == currentUid) {
+        return;
     }
-  });
-};
-
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('loaded').style.display = 'block';
+    user ? handleSignedInUser(user) : handleSignedOutUser();
+});
 
 /**
  * Initializes the app.
  */
-let initApp = function() {
-
-  // document.getElementById('sign-in-with-redirect').addEventListener(
-  //     'click', signInWithRedirect);
-  // document.getElementById('sign-in-with-popup').addEventListener(
-  //     'click', signInWithPopup);
-  document.getElementById('sign-out').addEventListener('click', function() {
-    firebase.auth().signOut();
-  });
-  // document.getElementById('delete-account').addEventListener(
-  //     'click', function() {
-  //       deleteAccount();
-  // });
+let initApp = function () {
+    document.getElementById('sign-out').addEventListener('click', function () {
+        firebase.auth().signOut();
+    });
 };
 
 window.addEventListener('load', initApp);
