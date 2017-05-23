160b9f1f1a21439327bb76af15cf635a16f0aa2e
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-05-22 19:07:44 -0700

    added timeout to resize on orientation change

--- screen.js
+++ screen.js
@@ -2,6 +2,8 @@
 
 // Adjusts game frame size. Mostly to fix scrollbars on Chrome.
 
+let resizeTimeout;
+
 function resizeIframe() {
     let iframe = document.getElementById("game-window");
     if(window.innerWidth > window.innerHeight) {
@@ -11,7 +13,13 @@ function resizeIframe() {
         iframe.style.height = window.innerWidth + "px";
         iframe.style.width = window.innerHeight + "px";
     }
+    console.log("hello");
+}
+
+function waitToResize() {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(resizeIframe, 500);
 }
 
 window.addEventListener("resize", resizeIframe);
-window.addEventListener("orientationchange", resizeIframe);
+window.addEventListener("orientationchange", waitToResize);
