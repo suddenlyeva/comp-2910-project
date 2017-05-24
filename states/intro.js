d6692e4a7b642f7a6441f72c680cddf26bdfebcd
Konstantin Kuchkov <konstantin.kuchkov@gmail.com>
2017-05-24 16:09:35 -0700

    added expanding team19 logo

--- states/intro.js
+++ states/intro.js
@@ -15,6 +15,11 @@ function Intro() {
     // FACTORY
     let txtFactory = new PIXI.Text("FACTORY", txtFood.style);
 
+    let contFoodFactory = new PIXI.Container();
+    contFoodFactory.addChild(txtFood);
+    txtFactory.x = contFoodFactory.width + TILES_PX / 1.85;
+    contFoodFactory.addChild(txtFactory);
+
     // ZERO
     let txtZero = new PIXI.Text("ZERO", txtFood.style.clone());
     txtZero.style.fontSize = 112;
@@ -31,20 +36,28 @@ function Intro() {
     let txtTeam19 = new PIXI.Text("by team19", {
         fontFamily: FONT_FAMILY, fontSize: 64, fill: 0x333333
     });
+    let teamBg = new PIXI.Sprite(PIXI.loader.resources["images/spritesheet.json"].textures["fade-backing.png"]);
+
+    let byTeam19 = new PIXI.Container();
+    byTeam19.addChild(teamBg);
+    teamBg.scale.set(1.5, 1);
+    txtTeam19.position.set(byTeam19.width / 2 - txtTeam19.width / 2 + 5, byTeam19.height / 2 - txtTeam19.height / 2 - 5)
+    byTeam19.addChild(txtTeam19);
 
     // Position text
     // note no padding between lines - because font has spacing already
 
-    // FOOD
-    txtFood.position.set(TILES_PX * 4, 0);
-    // FACTORY
-    txtFactory.position.set(txtFood.x + txtFood.width + TILES_PX / 2, txtFood.y);
+    // FOOD FACTORY
+    contFoodFactory.position.set(TILES_PX * 4, 0);
     // ZERO
-    txtZero.position.set(CANVAS_WIDTH / 2 - txtZero.width / 2, txtFactory.y + txtFactory.height);
+    txtZero.position.set(CANVAS_WIDTH / 2 - txtZero.width / 2, contFoodFactory.y + contFoodFactory.height);
     // press anywhere to continue...
     txtPress.position.set(CANVAS_WIDTH / 2 - txtPress.width / 2, CANVAS_HEIGHT - txtPress.height - 10);
     // by team19
-    txtTeam19.position.set(CANVAS_WIDTH - txtTeam19.width - 10, CANVAS_HEIGHT - txtTeam19.height - 10);
+    byTeam19.position.set(CANVAS_WIDTH - byTeam19.width, CANVAS_HEIGHT - byTeam19.height);
+
+    // reference coordinates for positioning the scaled down container
+    let team19Coord = { refX: byTeam19.x + byTeam19.width / 2, refY: byTeam19.y + byTeam19.height / 2 };
 
     // Gear ------------------------------------------------------------
     let gearSpin = {
@@ -59,11 +72,11 @@ function Intro() {
     spinningGear.updateSpeed = () => {
         if(!gearSpin.max.reached) {
             speedDiff               = gearSpin.max.val - spinningGear.speed;
-            spinningGear.speed         += speedDiff / gearSpin.decel * TICKER.deltaTime;
+            spinningGear.speed     += speedDiff / gearSpin.decel * TICKER.deltaTime;
             gearSpin.max.reached    = Math.abs(speedDiff) < gearSpin.epsilon;
         } else if(!gearSpin.final.reached) {
             speedDiff               = gearSpin.final.val - spinningGear.speed;
-            spinningGear.speed         += speedDiff / gearSpin.decel * TICKER.deltaTime;
+            spinningGear.speed     += speedDiff / gearSpin.decel * TICKER.deltaTime;
             gearSpin.final.reached  = Math.abs(speedDiff) < gearSpin.epsilon;
         }
     };
@@ -75,7 +88,7 @@ function Intro() {
         decel     : 10,
     };
 
-    let logoApple = new PIXI.Sprite(PIXI.utils.TextureCache["constantine-large.png"]);
+    let logoApple = new PIXI.Sprite(PIXI.loader.resources["images/spritesheet.json"].textures["constantine-large.png"]);
     let gearAppleContainer = new PIXI.Container();
     gearAppleContainer.addChild(spinningGear);
     gearAppleContainer.addChild(logoApple);
@@ -87,8 +100,8 @@ function Intro() {
     gearAppleContainer.updatePos = () => {
         let posDiff = gearMove.finalY.val - gearAppleContainer.y;
         if(!gearMove.finalY.reached) {
-            gearMove.finalY.reached = Math.abs(posDiff) < gearMove.epsilon;
-            gearAppleContainer.y += (posDiff / gearMove.decel) * TICKER.deltaTime;
+            gearMove.finalY.reached  = Math.abs(posDiff) < gearMove.epsilon;
+            gearAppleContainer.y    += (posDiff / gearMove.decel) * TICKER.deltaTime;
         }
     };
 
@@ -108,45 +121,52 @@ function Intro() {
 
     let foodBurst = makeItemBurst(gearAppleContainer.x + gearAppleContainer.width / 2, gearMove.finalY.val + gearAppleContainer.height / 2);
 
+    let background = new PIXI.Sprite(PIXI.utils.TextureCache["images/background-intro.png"]);
+
     // Add to scene
     this.scene = new PIXI.Container();
+    this.scene.addChild(background);
     this.scene.addChild(foodBurst);
-    this.scene.addChild(txtFood);
-    this.scene.addChild(txtFactory);
+    this.scene.addChild(contFoodFactory);
     this.scene.addChild(txtZero);
     this.scene.addChild(txtPress);
-    this.scene.addChild(txtTeam19);
+    this.scene.addChild(byTeam19);
     this.scene.addChild(gearAppleContainer);
     this.scene.addChild(clickableArea);
 
     // Start invisible
-    txtFood   .alpha = 0;
-    txtFactory.alpha = 0;
-    txtZero   .alpha = 0;
-    txtPress  .alpha = 0;
+    contFoodFactory.alpha = 0;
+    txtZero        .alpha = 0;
+    txtPress       .alpha = 0;
+    byTeam19       .scale.set(0);
 
     // Controls for appearing text
     let appearSpeed = 0.02;
     let flashFreq   = 0.03;
     let counter     = 0;
+    let scaleDecel  = 7;
     
     // Update the scene
     this.update = () => {
-        if(txtFood.alpha < 1 && txtFactory.alpha < 1) {
-            txtFood.alpha    += appearSpeed * TICKER.deltaTime;
-            // } else if(txtFactory.alpha < 1) {
-            txtFactory.alpha += appearSpeed * TICKER.deltaTime; // Each line shows one at a time
+        if(contFoodFactory.alpha < 1) {
+            contFoodFactory.alpha += appearSpeed * TICKER.deltaTime;
         } else if(!gearMove.finalY.reached) {
             gearAppleContainer.updatePos();
         } else { // Then the continue text flashes based on a sin function
             txtPress.alpha = Math.pow(Math.sin(counter), 4);
             counter        = (counter + flashFreq * TICKER.deltaTime) % Math.PI;
             if(txtZero.alpha < 1) {
-                txtZero.alpha    += appearSpeed * TICKER.deltaTime;
+                txtZero.alpha += appearSpeed * TICKER.deltaTime;
             }
             spinningGear.updateSpeed();
             if(gearSpin.max.reached) {
                 foodBurst.update();
+                if(byTeam19.scale.x < 1) {
+                    byTeam19.scale.set(byTeam19.scale.x + (1 - byTeam19.scale.x) / scaleDecel * TICKER.deltaTime);
+                    byTeam19.position.set(
+                        team19Coord.refX - byTeam19.width / 2,
+                        team19Coord.refY - byTeam19.height / 2);
+                }
             }
         }
         spinningGear.update();
